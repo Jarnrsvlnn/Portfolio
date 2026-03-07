95f4213be9b99db0915126720c9691277eb14c1b
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2026-03-07 09:55:17 +0800

    Implemented the base layout of the index

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+
+    {{-- ABOUT SECTION --}}
+    <div class="border-2 border-[rgb(120,120,120)] rounded-xl flex flex-col gap-3 px-4 py-7">
+        {{-- UPPER CONTAINER --}}
+        <div class="border">
+            <h1 class="text-white text-[clamp(2rem,2.5vw,2.5rem)] font-bold">John Robert Sevillena</h1>
+
+            <div class="flex gap-1">
+                <svg viewBox="-3 0 20 20" class="size-5" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>pin_sharp_circle [#624]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-223.000000, -5439.000000)" fill="#ffffff"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M176,5286.219 C176,5287.324 175.105,5288.219 174,5288.219 C172.895,5288.219 172,5287.324 172,5286.219 C172,5285.114 172.895,5284.219 174,5284.219 C175.105,5284.219 176,5285.114 176,5286.219 M174,5296 C174,5296 169,5289 169,5286 C169,5283.243 171.243,5281 174,5281 C176.757,5281 179,5283.243 179,5286 C179,5289 174,5296 174,5296 M174,5279 C170.134,5279 167,5282.134 167,5286 C167,5289.866 174,5299 174,5299 C174,5299 181,5289.866 181,5286 C181,5282.134 177.866,5279 174,5279" id="pin_sharp_circle-[#624]"> </path> </g> </g> </g> </g></svg>
+                <h2 class="text-white">Cavite, Philippines</h2>
+            </div>
+        </div>
+
+        {{-- LOWER CONTAINER --}}
+        <div class="border">
+            <p class="text-white text-justify">Hi! I'm a 20-years-old student from the Philippines, I mainly do <span class="text-yellow-300">web development</span>, and does <span class="text-yellow-300">video editing</span> and <span class="text-yellow-300">UI/UX designing</span> as creative hobbies.</p>
+        </div>
+    </div>
+
+    {{-- TECH STACK --}}
+    <div>
+        
+    </div>
+
+</x-layout>
