c30f1fb36b44046ede7b19f94047046d88edb8c2
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2026-03-09 09:20:42 +0800

    Improved the UX for about me description

--- resources/views/components/description-text.blade.php
+++ resources/views/components/description-text.blade.php
@@ -1,3 +1,3 @@
-<p {{ $attributes->merge(['class' => 'text-white text-justify']) }}>
+<p {{ $attributes->merge(['class' => 'text-white font-medium text-[clamp(1rem,2vw,1.2rem)] text-justify']) }}>
     {{ $slot }}
 </p>
