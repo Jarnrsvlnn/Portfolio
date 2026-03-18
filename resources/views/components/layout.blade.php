802c505ad1ec132ee928d0b20194dd8132eeb970
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2026-03-18 20:06:57 +0800

    Added intro animations

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,7 +22,7 @@
     <body class="bg-[rgb(10,10,10)] px-5 py-3 scroll-smooth">
 
         <div class="max-w-6xl mx-auto w-full">
-            <nav class="sticky top-0 w-full rounded-4xl p-5 min-w-80 sm:min-w-100 liquid-glass mb-15 md:mb-10">
+            <nav class="sticky liquid-panel top-3 left-0 w-full rounded-4xl p-5 min-w-80 sm:min-w-100 mb-15 md:mb-10 z-50 transition-all duration-500 opacity-100 translate-y-0 starting:opacity-50 starting:-translate-y-10">
                 <div class="flex flex-row gap-5 justify-between text-white mx-3 sm:mx-10">
                     <a href="#about" class="flex gap-2 cursor-pointer">
                         <svg class="block size-6" viewBox="0 0 512 512" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>about</title> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="about-white" fill="#ffffff" transform="translate(42.666667, 42.666667)"> <path d="M213.333333,3.55271368e-14 C95.51296,3.55271368e-14 3.55271368e-14,95.51168 3.55271368e-14,213.333333 C3.55271368e-14,331.153707 95.51296,426.666667 213.333333,426.666667 C331.154987,426.666667 426.666667,331.153707 426.666667,213.333333 C426.666667,95.51168 331.154987,3.55271368e-14 213.333333,3.55271368e-14 Z M213.333333,384 C119.227947,384 42.6666667,307.43872 42.6666667,213.333333 C42.6666667,119.227947 119.227947,42.6666667 213.333333,42.6666667 C307.44,42.6666667 384,119.227947 384,213.333333 C384,307.43872 307.44,384 213.333333,384 Z M240.04672,128 C240.04672,143.46752 228.785067,154.666667 213.55008,154.666667 C197.698773,154.666667 186.713387,143.46752 186.713387,127.704107 C186.713387,112.5536 197.99616,101.333333 213.55008,101.333333 C228.785067,101.333333 240.04672,112.5536 240.04672,128 Z M192.04672,192 L234.713387,192 L234.713387,320 L192.04672,320 L192.04672,192 Z" id="Shape"> </path> </g> </g> </g></svg>
@@ -40,7 +40,7 @@
                         <svg class="pt-1 block size-6" fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M4,21a1,1,0,0,0,1,1H19a1,1,0,0,0,1-1V3a1,1,0,0,0-1-1H5A1,1,0,0,0,4,3ZM12,7.5a2,2,0,1,1-2,2A2,2,0,0,1,12,7.5ZM8.211,16.215a4,4,0,0,1,7.578,0A.993.993,0,0,1,14.83,17.5H9.18A1,1,0,0,1,8.211,16.215Z"></path></g></svg>
                         <span class="hidden md:block">Contact</span>
                     </a>
-                </div>
+                </div>  
             </nav>
             
             <main class="flex flex-col justify-center sm:px-40 w-full min-w-80 gap-y-20">
--- resources/views/components/title-text.blade.php
+++ resources/views/components/title-text.blade.php
@@ -1,3 +1,3 @@
 <h1 {{ $attributes->merge(['class' => 'text-white text-[clamp(2rem,2.5vw,2.5rem)] font-bold']) }}>
     {{ $slot }}
-</h1>
+</h1>   
