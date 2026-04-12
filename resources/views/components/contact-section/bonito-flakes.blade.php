19ee3c6f79441707769064ba1fd288feac48ab89
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2026-04-12 09:36:32 +0800

    Improved the responsiveness of the contact page, added some hover effects and suited icons

--- resources/views/components/contact-section/bonito-flakes.blade.php
+++ resources/views/components/contact-section/bonito-flakes.blade.php
@@ -4,11 +4,11 @@
     ])
 
 @if ($bonito == 'social')
-    <a href="{{ $link }}" target="_blank" rel="noopener noreferrer" class="flex flex-col space-y-0.5 justify-center w-full cursor-pointer transition-all duration-50 ease-out hover:border-3 hover:border-green-400 rounded-sm">
+    <a href="{{ $link }}" target="_blank" rel="noopener noreferrer" class="group flex flex-row justify-between items-center p-1 w-full cursor-pointer transition-all duration-50 ease-out hover:bg-green-600 rounded-sm">
         {{ $slot }}
     </a>
 @else
-    <div id="open-dialog" class="flex flex-col space-y-0.5 justify-center w-full cursor-pointer p-1 transition-all duration-50 ease-out hover:border-3 hover:border-green-400 rounded-sm">
+    <div id="open-dialog" class="group flex flex-row justify-between items-center w-full cursor-pointer p-1 transition-all duration-50 ease-out hover:bg-green-600 rounded-sm">
         {{ $slot }}
     </div>
 @endif
